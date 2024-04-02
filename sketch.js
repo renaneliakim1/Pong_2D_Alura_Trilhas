2ab54abbdfec50ef07772f942219320637d69e0f
renaneliakim1 <renaneliakim1@gmail.com>
2024-04-02 10:25:55 -0300

    including sounds, scoring functions and bug fixes

--- sketch.js
+++ sketch.js
@@ -11,15 +11,19 @@
 //raquete colisão biblioteca = libraryCollisionRacket
 //colisão =  collision
 //mover raquete oponente = moveOpponentRacket
-//y velocidade  do oponente= ySpeedyOpponent
+//y velocidade  do oponente= ySpeedOpponent
 // raquete oponente colisão biblioteca = opponentCollisionRacketLibrary
 //verifica colisão raquete = checksRacketCollision
+// pontos = score
+//pontuação oponente= opponentScore
+//placar = scoreboard
+
 
 let xBall= 300;
 let yBall= 200;
-let diameter= 15;
-let xSpeedxBall= 2;
-let ySpeedyBall= 2;
+let diameter= 12;
+let xSpeedxBall= 8;
+let ySpeedyBall= 8;
 let ray = diameter / 2;
 
 let xRacket = 5;
@@ -29,16 +33,32 @@ let xOpponentRacket = 585;
 let yOpponentRacket = 150;
 
 
-let ySpeedyOpponent;
+let ySpeedOpponent;
 
 let racketLength = 10;
 let racketHeight = 100;
 let collision = false;
  
+let score = 0;
+let opponentScore = 0;
+
+let racketSound;
+let soundTrack;
+let dotSound;
+
+
+function preload(){
+  soundTrack = loadSound("/Pong-Sons/Track.mp3");
+  racketSound = loadSound("/Pong-Sons/Racket.mp3");
+  dotSound = loadSound("/Pong-Sons/Point.mp3");
+
+
+};
 
 
 function setup (){
   createCanvas(600, 400);
+  soundTrack.loop();
 }
 
 function draw (){
@@ -47,13 +67,19 @@ function draw (){
   moveBall();
   checkEdgeCollision();
   showRacket(xRacket, yRacket);
-  moverRaquete1();
-  //checkRacketCollision();
+  moveRacket1();
+  checkRacketCollision();
   checksRacketCollision(xRacket,yRacket);
   showRacket(xOpponentRacket, yOpponentRacket);
   moveOpponentRacket();
   checksRacketCollision(xOpponentRacket, yOpponentRacket);
   showOpponentRacket();
+  scoreboard();
+  deeppointCount();
+
+
+
+
 }
 
 function showBall(){
@@ -84,7 +110,7 @@ function showRacket(x,y){
 };
 
 
-function moverRaquete1(){
+function moveRacket1(){
   if (keyIsDown(UP_ARROW)){       // write the same js documentation
     yRacket -= 10;
   };
@@ -97,6 +123,7 @@ function moverRaquete1(){
 function checkRacketCollision(){
   if(xBall - ray < xRacket + racketLength && yBall - ray < yRacket + racketHeight && yBall + ray > yRacket){
     xSpeedxBall *= -1;
+    t
   }
 };
 
@@ -105,14 +132,42 @@ function checksRacketCollision(x,y){
   collideRectCircle(x, y, racketLength, racketHeight, xBall, yBall, ray);
   if (collision){
     xSpeedxBall *= -1;
-
+    racketSound.play();
   }
 }
 
 
 function moveOpponentRacket(){
-  ySpeedyOpponent= yBall - yOpponentRacket - yOpponentRacket - racketLength / 2 -30;
-  yOpponentRacket += ySpeedyOpponent;
+  ySpeedOpponent= yBall - yOpponentRacket - racketLength / 2 -30;
+  yOpponentRacket += ySpeedOpponent;
+  racketSound.play();
 
  
 }
+
+function scoreboard(){
+  stroke(255);
+  textAlign(CENTER);
+  textSize(16);
+  fill(color(255,140,0));
+  rect(200, 10, 40, 20);
+  fill(255);
+  text(score, 220 , 26);
+  fill(color(255,140,0));
+  rect(400, 10, 40, 20);
+  fill(255);
+  text(opponentScore,  420, 26);
+
+}
+
+function deeppointCount(){
+  if( xBall > 590){
+    score += 1;
+    dotSound.play();
+  }
+  if(xBall< 10){
+    opponentScore += 1;
+    dotSound.play();
+  }
+};
+
